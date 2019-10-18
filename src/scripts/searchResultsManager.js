34d9cf54910aaffc93b7e0190285c6e4bb7bb2a8
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-18 14:37:36 -0500

    commented searchResultsManager.js

--- src/scripts/searchResultsManager.js
+++ src/scripts/searchResultsManager.js
@@ -1,3 +1,4 @@
+// This function returns a string template of what the HTML will be for one recipe.
 const buildRecipeHtml = recipe => `
 <article>
   <h4>${recipe.title}</h4>
@@ -8,14 +9,19 @@ const buildRecipeHtml = recipe => `
 </article>
 `
 
-
+// this function takes in an array of recipes, iterates over them to turn each recipe into a string template for HTML, and then displays them on the DOM
 const displayRecipeHtml = allRecipes => {
+  // Empty string to build up
   let recipeResultsHtml = ""
   allRecipes.forEach(recipe => {
+    // takes current recipe, makes it into an HTML string
     let recipeHtml = buildRecipeHtml(recipe)
+    // adds current html string (that represents a recipe) into the running string of recipes called "recipesResultsHtml"
     recipeResultsHtml += recipeHtml
   });
 
+  // targets the div on the DOM with the class "search-resuls"
   const searchResultsSection = document.querySelector(".search-results")
+  // changes the innerHTML of the "search-results" div to be what is stored in the variable recipeResultsHtml
   searchResultsSection.innerHTML = recipeResultsHtml
 }
