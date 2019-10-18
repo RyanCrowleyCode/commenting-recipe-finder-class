e9ec028245dfe09ac15f9bdc54b650c66346ee00
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-18 14:48:07 -0500

    commented searchFormManager.js

--- src/scripts/searchFormManager.js
+++ src/scripts/searchFormManager.js
@@ -1,4 +1,6 @@
+// This function builds a string template for 2 html sections and places them on the DOM
 const buildAndAppendSearchForm = () => {
+  // the variable searchForm is a string template of the html for 2 sections. One of those sections contains a header, an input field, and a button. The other section is empty for now.
   const searchForm = `
   <section class="search-form">
     <h3>Search for recipes containing: </h3>
@@ -7,8 +9,9 @@ const buildAndAppendSearchForm = () => {
   </section>
   <section class="search-results"></section>
   `
-  // console.log(searchForm)
 
+  // this variable targets the container div on index.html so that we can manipulate the DOM
   const containerDiv = document.querySelector("#container")
+  // here, we are updating that container div to now have the content defined in the searchForm variable above, on the DOM
   containerDiv.innerHTML = searchForm
 }
