c2bc18b06434a5cd447f90f1d35430cce13eab53
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-18 14:28:31 -0500

    commented index.html, main.css, and main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
+// This console.log is to check and see if main.js is linked to index.html
 console.log("The recipe finder file structure is set up!")
 
+// This code runs the buildAndAppendSearchForm() function. This function is defined in searchFormManager.js
 buildAndAppendSearchForm()
+// This code runs the attachEventListenerToSearchButton() function. This function is defined in eventManager.js
 attachEventListenerToSearchButton()
